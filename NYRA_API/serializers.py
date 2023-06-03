8b53c84c26eff0dad55b884738908e88e2ebea20
cmbbaldini <cmbbaldini@gmail.com>
2023-06-03 16:48:02 -0700

    revert folder name change

--- NYRA_API/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Race
-
-class RaceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Race
-        fields = ['date', 'track', 'raceNumber', 'surface', 'distance', 
-                  'condition', 'fieldSize', 'maidens', 'firstCall', 'secondCall']
