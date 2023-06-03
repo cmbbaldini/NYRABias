d442182e0a06280357a4805646c2dee874af947a
cmbbaldini <cmbbaldini@gmail.com>
2023-06-02 17:02:11 -0700

    fix error in settings

--- NYRABias/settings.py
+++ NYRABias/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
-        ...
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
