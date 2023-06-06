09e50de2150bb227259768d9dcbdefca8857fae7
cmbbaldini <cmbbaldini@gmail.com>
2023-06-06 03:07:19 -0700

    update filters import to use rest framwork filters

--- NYRABias/filters.py
+++ NYRABias/filters.py
@@ -1,15 +1,15 @@
-import django_filters
+from django_filters import rest_framework as filters
 from .models import Race
 
-class RaceFilter(django_filters.FilterSet):
-    date = django_filters.DateFromToRangeFilter(field_name="date")
-    track = django_filters.CharFilter(field_name= "track", lookup_expr='iexact')
-    surface = django_filters.CharFilter(field_name="surface", lookup_expr='iexact')
-    distance = django_filters.CharFilter(field_name="distance", lookup_expr='iexact')
-    condition = django_filters.CharFilter(field_name="condition", lookup_expr='iexact')
-    fieldSize_lt = django_filters.NumberFilter(field_name="fieldSize", lookup_expr="lt")
-    fieldSize_gt = django_filters.NumberFilter(field_name="fieldSize", lookup_expr="gt")
-    maidens = django_filters.BooleanFilter(field_name="maidens")
+class RaceFilter(filters.FilterSet):
+    date = filters.DateFromToRangeFilter(field_name="date")
+    track = filters.CharFilter(field_name= "track", lookup_expr='iexact')
+    surface = filters.CharFilter(field_name="surface", lookup_expr='iexact')
+    distance = filters.CharFilter(field_name="distance", lookup_expr='iexact')
+    condition = filters.CharFilter(field_name="condition", lookup_expr='iexact')
+    fieldSize_lt = filters.NumberFilter(field_name="fieldSize", lookup_expr="lt")
+    fieldSize_gt = filters.NumberFilter(field_name="fieldSize", lookup_expr="gt")
+    maidens = filters.BooleanFilter(field_name="maidens")
 
     class Meta:
         model = Race
