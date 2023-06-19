e953368a88daee2970ba59c5a2ffb42bb04ace64
cmbbaldini <cmbbaldini@gmail.com>
2023-06-19 02:27:17 -0700

    update field names for first call

--- NYRABias/models.py
+++ NYRABias/models.py
@@ -10,8 +10,8 @@ class Race(models.Model):
     condition = models.CharField(max_length=25)
     fieldSize = models.IntegerField(default=0)
     maidens = models.BooleanField(default=False)
-    firstCallPosition = models.IntegerField(default=0)
-    secondCallPosition = models.IntegerField(default=0)
+    firstCall = models.IntegerField(default=0)
+    secondCall = models.IntegerField(default=0)
     firstCallLengthsBehind = models.IntegerField(default=0)
     secondCallLengthsBehind = models.IntegerField(default=0)
 
