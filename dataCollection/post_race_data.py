1d5231af713c5edf8c7b7dc32459fcd8a2834851
cmbbaldini <cmbbaldini@gmail.com>
2023-06-20 02:21:29 -0700

    add backend data collection

--- dataCollection/post_race_data.py
+++ dataCollection/post_race_data.py
@@ -2,8 +2,8 @@ import json
 import requests
 
 def postRace(data):
-    url = 'https://nyra-bias.onrender.com/post/'
+    url = 'MyURL'
     data = json.dumps(data)
-    headers = {'Content-type': 'application/json', "Authorization": "Token 9e8347a90c0772afd666140e45233fd7bbd995d2"}
+    headers = {'Content-type': 'application/json', "Authorization": "Token MyToken"}
     r = requests.post(url, data=data, headers=headers)
     print(r.content)
