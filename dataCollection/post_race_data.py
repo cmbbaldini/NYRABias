3cdc4010449ceec473da392424851f67626156e0
cmbbaldini <cmbbaldini@gmail.com>
2023-06-20 03:44:18 -0700

    update url

--- dataCollection/post_race_data.py
+++ dataCollection/post_race_data.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 def postRace(data):
-    url = 'MyURL'
+    url = 'https://nyra-bias.onrender.com/post/'
     data = json.dumps(data)
     headers = {'Content-type': 'application/json', "Authorization": "Token MyToken"}
     r = requests.post(url, data=data, headers=headers)
